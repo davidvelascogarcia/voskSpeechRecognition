3733e4e92f596939ce30d229af8784f7f546f946
davidvelascogarcia <davelasc@ing.uc3m.es>
2020-07-25 21:34:18 +0200

    Fix format

--- programs/voskSpeechRecognition.py
+++ programs/voskSpeechRecognition.py
@@ -36,21 +36,24 @@ print("*************************************************************************
 
 print("")
 print("Starting system ...")
+print("")
 
 print("")
 print("Loading Vosk Speech Recognition module ...")
+print("")
 
 # Get system configuration
 print("")
 print("Detecting system and release version ...")
+print("")
 systemPlatform = platform.system()
 systemRelease = platform.release()
 
 print("")
-print("")
 print("**************************************************************************")
 print("Configuration detected:")
 print("**************************************************************************")
+print("")
 print("Platform:")
 print(systemPlatform)
 print("Release:")
@@ -60,10 +63,14 @@ print("")
 print("**************************************************************************")
 print("Language configuration:")
 print("**************************************************************************")
+print("")
 
+# Variable to loopControlFileExists
 loopControlFileExists = 0
 
 while int(loopControlFileExists)==0:
+
+    # If file founded
     try:
         # Get language config
         print("")
@@ -75,22 +82,24 @@ while int(loopControlFileExists)==0:
 
         userLanguage = languageConfigurationObject['Language']['user-language']
 
-        print("User language: " + str(userLanguage))
+        print("")
+        print("[INFO] User language: " + str(userLanguage))
         print("")
 
+        # Set loopControlFileExists
         loopControlFileExists = 1
 
+    # If file not founded
     except:
         print("")
         print("[ERROR] Sorry, language.ini not founded, waiting 4 seconds to the next check ...")
         print("")
         time.sleep(4)
 
-
+print("")
 print("[INFO] Data obtained correctly.")
 print("")
 
-print("")
 print("")
 print("**************************************************************************")
 print("Loading language model:")
@@ -99,10 +108,12 @@ print("")
 print("[INFO] Loading "+ str(userLanguage) + " language model ...")
 print("")
 
+# Variable to loopControlModelExist
 loopControlModelExist = 0
 
 while int(loopControlModelExist) == 0:
 
+    # If model exist
     try:
         # Get full model path
         modelPath = "./../models/model-" + str(userLanguage)
@@ -113,17 +124,16 @@ while int(loopControlModelExist) == 0:
         print("[INFO] Model loaded correctly.")
         print("")
 
+        # Set loopControlModelExist
         loopControlModelExist = 1
 
+    # If model not located
     except:
         print("")
         print("[ERROR] Model not founded, next check in 4 seconds.")
         print("")
         time.sleep(4)
 
-
-
-print("")
 print("")
 print("**************************************************************************")
 print("Initializing voskSpeechRecognitionEngine:")
@@ -132,21 +142,25 @@ print("")
 print("[INFO] Initializing voskSpeechRecognitionEngine ...")
 print("")
 
+# Variable to loopControlInitEngine
 loopControlInitEngine = 0
 
 while int(loopControlInitEngine) == 0:
 
     try:
-
         # Init kaldi recognizer
         print("")
         print("Initializing engine ...")
         print("")
+
+        # Build engine
         voskSpeechRecognitionEngine = KaldiRecognizer(voskSpeechRecognitionModel, 16000)
+
         print("")
         print("[INFO] voskSpeechRecognitionEngine initialized correctly")
         print("")
 
+        # Set loopControlInitEngine
         loopControlInitEngine = 1
 
     except:
@@ -155,9 +169,6 @@ while int(loopControlInitEngine) == 0:
         print("")
         time.sleep(4)
 
-
-
-print("")
 print("")
 print("**************************************************************************")
 print("Initializing microphone:")
@@ -166,6 +177,7 @@ print("")
 print("[INFO] Initializing microphone ...")
 print("")
 
+# Variable to loopControlInitMicrophone
 loopControlInitMicrophone = 0
 
 while int(loopControlInitMicrophone) == 0:
@@ -188,6 +200,7 @@ while int(loopControlInitMicrophone) == 0:
         print("[INFO] Microphone initialized correctly")
         print("")
 
+        # Set loopControlInitMicrophone
         loopControlInitMicrophone = 1
 
     except:
@@ -196,41 +209,42 @@ while int(loopControlInitMicrophone) == 0:
         print("")
         time.sleep(4)
 
-print("")
+
 print("")
 print("**************************************************************************")
 print("YARP configuration:")
 print("**************************************************************************")
 print("")
 print("Initializing YARP network ...")
+print("")
 
 # Init YARP Network
 yarp.Network.init()
 
-
 print("")
 print("[INFO] Opening data input port with name /voskSpeechRecognition/data:i ...")
+print("")
 
 # Open input voskSpeechRecognition port
 voskSpeechRecognition_inputPort = yarp.Port()
 voskSpeechRecognition_inputPortName = '/voskSpeechRecognition/data:i'
 voskSpeechRecognition_inputPort.open(voskSpeechRecognition_inputPortName)
 
-# Create input data bottle
-inputBottle=yarp.Bottle()
+# Create voskSpeechRecognition input data bottle
+voskSpeechRecognitionInputBottle = yarp.Bottle()
 
 print("")
 print("[INFO] Opening data output port with name /voskSpeechRecognition/data:o ...")
+print("")
 
 # Open output voskSpeechRecognition port
 voskSpeechRecognition_outputPort = yarp.Port()
 voskSpeechRecognition_outputPortName = '/voskSpeechRecognition/data:o'
 voskSpeechRecognition_outputPort.open(voskSpeechRecognition_outputPortName)
 
-# Create output data bottle
+# Create voskSpeechRecognition output data bottle
 voskSpeechRecognitionOutputBottle = yarp.Bottle()
 
-print("")
 print("")
 print("**************************************************************************")
 print("Waiting for input audio:")
@@ -239,24 +253,21 @@ print("")
 print("[INFO] Waiting for input audio ...")
 print("")
 
+# Variable to loopControlAnalyzingAudio
 loopControlAnalyzingAudio = 0
 
 while int(loopControlAnalyzingAudio) == 0:
 
     print("")
-    print("")
     print("**************************************************************************")
     print("Recognizing audio:")
     print("**************************************************************************")
     print("")
-    print("[INFO] Listening audio ...")
+    print("[INFO] Listening audio at " + str(datetime.datetime.now()) + " ...")
     print("")
 
     # Listened audio
     listenedAudio = voskSpeechRecognitionMicrophone.read(4000)
-
-    # Get detected datetime
-    recognizedTimeDetection = str(datetime.datetime.now())
 
     # If listenedAudio is empty
     if len(listenedAudio) == 0:
@@ -272,20 +283,21 @@ while int(loopControlAnalyzingAudio) == 0:
         # Print recognized results
         print("")
         print("[RESULTS] Recognized results:")
-        print("Recognized audio at " + str(recognizedTimeDetection) + ".")
+        print("Recognized audio at " + str(datetime.datetime.now()) + ".")
         print("")
+
+        # Print recognizedResults
         print(recognizedResults)
 
         # Prepare recognized text to send
         parsedRecognizedResults = recognizedResults.split('"text" : "')[1].split('"')[0]
 
-        # Send results with yarp port
+        # Send results with yarp voskSpeechRecognition_outputPort port
         voskSpeechRecognitionOutputBottle.clear()
-        voskSpeechRecognitionOutputBottle.addString("Recognized: ")
+        voskSpeechRecognitionOutputBottle.addString("RECOGNIZED:")
         voskSpeechRecognitionOutputBottle.addString(str(parsedRecognizedResults))
-        voskSpeechRecognitionOutputBottle.addString("Time: ")
-        voskSpeechRecognitionOutputBottle.addString(recognizedTimeDetection)
-
+        voskSpeechRecognitionOutputBottle.addString("DATE:")
+        voskSpeechRecognitionOutputBottle.addString(str(datetime.datetime.now()))
         voskSpeechRecognition_outputPort.write(voskSpeechRecognitionOutputBottle)
 
     # If detect and recognize parcial results
@@ -293,17 +305,17 @@ while int(loopControlAnalyzingAudio) == 0:
 
         # Print partial results
         recognizedPartialResults = voskSpeechRecognitionEngine.PartialResult()
+
+        # Print partial results
         print(recognizedPartialResults)
 
-        # Send None results with yarp port
+        # Send None results with yarp voskSpeechRecognition_outputPort port
         voskSpeechRecognitionOutputBottle.clear()
-        voskSpeechRecognitionOutputBottle.addString("Recognized: ")
+        voskSpeechRecognitionOutputBottle.addString("RECOGNIZED:")
         voskSpeechRecognitionOutputBottle.addString("None")
-        voskSpeechRecognitionOutputBottle.addString("Time: ")
-        voskSpeechRecognitionOutputBottle.addString(recognizedTimeDetection)
-
+        voskSpeechRecognitionOutputBottle.addString("DATE:")
+        voskSpeechRecognitionOutputBottle.addString(str(datetime.datetime.now()))
         voskSpeechRecognition_outputPort.write(voskSpeechRecognitionOutputBottle)
-
 
 # Close YARP ports
 print("[INFO] Closing YARP ports ...")
